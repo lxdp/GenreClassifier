f82dedebd42559f26e15a30db8d002adc05c6284
Lucas Pedersen <lucasxdpedersen01@gmail.com>
2025-11-18 00:35:40 +0000

    Added try-except handling to capture IndexError exception

--- run_workflow.py
+++ run_workflow.py
@@ -1,10 +1,17 @@
 import sys
 
+# Utils
+from src.utils.loggers import GlobalTranslatorLogger
+
 from src.pipelines.service import GlobalWorkflow
 
 if __name__=="__main__":
+    
     # Provide genre and audio file from that genre as arguments when running 'python run_workflow.py'
-    genre = sys.argv[1]
-    audio_file = sys.argv[2]
-    global_workflow = GlobalWorkflow(genre, audio_file)
-    global_workflow.run_workflow()
+    try:
+        genre = sys.argv[1]
+        audio_file = sys.argv[2]
+        global_workflow = GlobalWorkflow(genre, audio_file)
+        global_workflow.run_workflow()
+    except IndexError as error:
+        GlobalTranslatorLogger.log_exception("CommandLineArgumentReadingFailure", "The necessary values in the command line were not provided")
