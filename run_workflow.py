c8970af9d2d8125a36c819451df0689265fa897f
Lucas Pedersen <lucasxdpedersen01@gmail.com>
2025-11-23 08:45:37 +0000

    Updated global workflow to take on api-endpoints

--- run_workflow.py
+++ run_workflow.py
@@ -1,17 +1,10 @@
 import sys
 
 # Utils
-from src.utils.loggers import GlobalTranslatorLogger
+from src.app.utils.loggers import GlobalTranslatorLogger
 
-from src.pipelines.service import GlobalWorkflow
+from src.app.pipelines.service import GlobalWorkflow
 
 if __name__=="__main__":
-    
-    # Provide genre and audio file from that genre as arguments when running 'python run_workflow.py'
-    try:
-        genre = sys.argv[1]
-        audio_file = sys.argv[2]
-        global_workflow = GlobalWorkflow(genre, audio_file)
-        global_workflow.run_workflow()
-    except IndexError as error:
-        GlobalTranslatorLogger.log_exception("CommandLineArgumentReadingFailure", f"The following error occured: {error}")
+    global_workflow = GlobalWorkflow()
+    global_workflow.run_workflow()
